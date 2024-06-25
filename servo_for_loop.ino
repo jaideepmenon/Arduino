2f2c4be4b0ca4f8bf995f7bc23c6600b21f1a2da
jaideepmenon <712jaideep@gmail.com>
2024-06-25 14:41:41 +0530

    servo_for_loop.ino

--- servo_for_loop.ino
+++ servo_for_loop.ino
@@ -11,11 +11,11 @@ void loop() {
   {
     myservo.write(pos);
     }
-    delay(15);
+    delay(1000);
     
  for(pos=180;pos>=0;pos-=1)
   {
     myservo.write(pos);
     }
-    delay(15);
+    delay(1000);
 }
